682c40305eafeeba0cc5e352f45d80240ccd89f3
GiannPogi <gcmediavillo@gmail.com>
2021-12-22 08:56:40 +0800

    Updated

--- src/views/app-view/UserProfileTest.js
+++ src/views/app-view/UserProfileTest.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./UserProfile.css"
 import { Form, Avatar, Button, Input, DatePicker, Row, Col, message, Upload, Card, } from 'antd';
+import Axios from "axios"
+
 
 function UserProfileTest() {
+  const [teacherID, setTeacherID] = useState("61c13a75cfd6f1012a2bd15f")
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [initialVal, setInitialVal] = useState([])
+
+  useEffect( () => {
+
+    Axios.get("http://localhost:5000/teacher/get/" + teacherID).then(
+      (response) => {
+        console.log(response.data);
+      }
+    )
+  }, [])
+
+  const updateTeacher = async (values) => {
+    await Axios.put(
+      "http://localhost:5000/teacher/update",
+      { values }
+    ).then((response) => {
+      console.log(response.data);
+    })
+
+    //alert("Teacher updated");
+  }
+
+
   return (
     <Row gutter={30} className="user-profile-container">
       <Col span={16}>
@@ -13,14 +42,16 @@ function UserProfileTest() {
             <Form
               name="basicInformation"
               layout="vertical"
+              onFinish={updateTeacher}
+              initialValues={initialVal}
             >
               <Row>
                 <Col xs={24} sm={24} md={24} lg={24}>
                   <Row gutter={10}>
                     <Col xs={24} sm={24} md={12}>
                       <Form.Item
-                        label={<p className="label-form-style">Name: </p>}
-                        name="name"
+                        label={<p className="label-form-style">First Name: </p>}
+                        name="first_name"
                         rules={[
                           {
                             required: true,
@@ -33,12 +64,12 @@ function UserProfileTest() {
                     </Col>
                     <Col xs={24} sm={24} md={12}>
                       <Form.Item
-                        label={<p className="label-form-style">Username: </p>}
-                        name="username"
+                        label={<p className="label-form-style">Last Name: </p>}
+                        name="last_name"
                         rules={[
                           {
                             required: true,
-                            message: 'Please input your username!'
+                            message: 'Please input your lastname!'
                           },
                         ]}
                       >
@@ -66,46 +97,6 @@ function UserProfileTest() {
                         <DatePicker className="w-100 form-input-style" />
                       </Form.Item>
                     </Col>
-                    <Col xs={24} sm={24} md={12}>
-                      <Form.Item
-                        label={<p className="label-form-style">Phone Number: </p>}
-                        name="phoneNumber"
-                      >
-                        <Input className="form-input-style" />
-                      </Form.Item>
-                    </Col>
-                    <Col xs={24} sm={24} md={12}>
-                      <Form.Item
-                        label={<p className="label-form-style">Website: </p>}
-                        name="website"
-                      >
-                        <Input className="form-input-style" />
-                      </Form.Item>
-                    </Col>
-                    <Col xs={24} sm={24} md={24}>
-                      <Form.Item
-                        label={<p className="label-form-style">Address: </p>}
-                        name="address"
-                      >
-                        <Input className="form-input-style" />
-                      </Form.Item>
-                    </Col>
-                    <Col xs={24} sm={24} md={12}>
-                      <Form.Item
-                        label={<p className="label-form-style">City: </p>}
-                        name="city"
-                      >
-                        <Input className="form-input-style" />
-                      </Form.Item>
-                    </Col>
-                    <Col xs={24} sm={24} md={12}>
-                      <Form.Item
-                        label={<p className="label-form-style">Post Code: </p>}
-                        name="postcode"
-                      >
-                        <Input className="form-input-style" />
-                      </Form.Item>
-                    </Col>
                   </Row>
                   <Button type="primary" htmlType="submit" className="form-submit-btn-style">
                     Save Change
@@ -131,11 +122,6 @@ function UserProfileTest() {
           <Avatar src={require("assets/img/faces/face-0.jpg").default} className="profile-picture-style" />
           <h4 className="text-center">Mike Andrew</h4>
           <p className="text-center">michael24</p>
-          <p className=" text-center">
-            "3rd year College Student <br></br>
-            Your chick she so thirsty <br></br>
-            I'm in that two seat Lambo"
-          </p>
 
         </Card>
       </Col>
