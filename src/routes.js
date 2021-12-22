b34210a50f253348c78b7c6a5d4e0af5f0a783d2
Rojhon <rojhonbuenaventura5@gmail.com>
2021-12-22 16:00:09 +0800

    Classroom

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,9 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import Dashboard from "views/app-view/Dashboard.js";
+
+import Classroom from "views/app-view/Classroom/Classroom";
+import CreateClassroom from "views/app-view/Classroom/CreateClassroom";
+
+
 import TableList from "views/app-view/TableList.js";
 import TestAntDesign from "./views/app-view/TestAntDesign";
 import UserProfileTest from "views/app-view/UserProfileTest";
@@ -35,10 +23,19 @@ const dashboardRoutes = [
     path: "/classroom",
     name: "Classroom",
     icon: "nc-icon nc-notes",
-    component: TableList,
+    component: Classroom,
     layout: "/admin",
     renderSidebar: true,
     isExact: true,
+  },
+  {
+    path: "/classroom/create-new",
+    name: "Classroom",
+    icon: "",
+    component: CreateClassroom,
+    layout: "/admin",
+    renderSidebar: false,
+    isExact: false,
   },
   {
     path: "/classroom/:id",
---
+++ src/views/app-view/Classroom/Classroom.js
@@ -0,0 +1,147 @@
+import React, {useState, useEffect} from 'react'
+import {
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
+import { Card, Table, Menu, Input, Button} from 'antd';
+import Flex from 'components/shared-components/Flex'
+import AvatarStatus from 'components/shared-components/AvatarStatus';
+import utils from 'utils'
+import EllipsisDropdown from 'components/shared-components/EllipsisDropdown'
+import { Link } from "react-router-dom";
+import { 
+	EyeOutlined,
+    SearchOutlined
+} from '@ant-design/icons';
+import axios from "axios";
+
+const Classroom = () => {
+    const userId = localStorage.getItem("mid");
+
+    console.log(userId)
+
+    const [classrooms, setClassrooms] = useState([]);
+    const [classroomsList, setClassroomsList] = useState([]);
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/teacher/get-classrooms/" + userId).then((response) => {
+            setClassrooms(response.data)
+            setClassroomsList(response.data)
+            setIsLoading(false);
+            setError(null);
+            console.log(response.data)
+          }).catch(() => {
+            setIsLoading(false);
+            setError("Could not fetch the data in the server!");
+          });
+    }
+    , []);
+
+    const tableColumns = [
+        {
+            title: 'Classroom Code',
+            dataIndex: 'class_code',
+            render: (_, record) => (
+                <span>{record.class_code}</span>
+            )
+        },
+        {
+            title: 'Classroom Name',
+            dataIndex: 'name',
+            render: (_, record) => (
+                <Flex>
+                    <AvatarStatus src="/img/thumbs/thumb-5.jpg" size={30} name={record.name}/>
+                </Flex>
+            ),
+            sorter: (a, b) => utils.antdTableSorter(a, b, 'name')
+        },
+        {
+            title: 'Section',
+            dataIndex: 'section_name',
+            render: (_, record) => (
+                <span>{record.section_name}</span>
+            )
+        },
+        ,
+        {
+            title: 'Actions',
+            dataIndex: 'actions',
+            render: (_, record) => (
+                <EllipsisDropdown 
+            menu={
+                <Menu>
+                    <Menu.Item key="0">
+                        <Link to={`classroom/${record.class_code}`}>
+                            <EyeOutlined />
+                            <span className="ml-2">View</span>
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item key="1">
+                        <Link to={`classroom/${record.class_code}`}>
+                            <EyeOutlined />
+                            <span className="ml-2">Update</span>
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        <Link to={`classroom/${record.class_code}`}>
+                            <EyeOutlined />
+                            <span className="ml-2">Delete</span>
+                        </Link>
+                    </Menu.Item>
+                </Menu>
+            }
+        />
+            )
+        }
+      ]
+
+    const onClassroomSearch = (e) => {
+        const value = e.currentTarget.value;
+        const searchArray = e.currentTarget.value ? classroomsList : classrooms;
+        const data = utils.wildCardSearch(searchArray, value);
+        setClassroomsList(data);
+    };
+    
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col md="12">
+            <Card title="Teacher Classroom" extra={<><Link to="classroom/create-new"><Button type="primary" >New Classroom</Button></Link></>}>
+              <Flex
+                alignItems="center"
+                className=""
+                justifyContent="between"
+                mobileFlex={false}
+              >
+                <Flex className="mb-1" mobileFlex={false}>
+                  <div className="mb-3 mr-md-3">
+                    <Input
+                      placeholder="Search"
+                      prefix={<SearchOutlined />}
+                      onChange={(e) => onClassroomSearch(e)}
+                    />
+                  </div>
+                </Flex>
+              </Flex>
+              <Table
+                  pagination={true}
+                  columns={tableColumns} 
+                  dataSource={classroomsList} 
+                  rowKey='id'
+                  loading={isLoading}
+                  scroll={{ x: "max-content" }}
+              />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default Classroom;
