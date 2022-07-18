8dd4f86c4f3429df31bb898cb42f1d6af1feb0ba
vonypeto <von.aralar@gmail.com>
2022-07-18 09:57:01 +0800

    added random thing

--- src/views/info-view/about.js
+++ src/views/info-view/about.js
@@ -40,37 +40,31 @@ const About = () => {
                   lg={18}
                   xl={18}
                 >
-                  <h2>About Us !</h2>
+                  <h2>About Us</h2>
                   <h3 style={{ textAlign: "center" }}>
-                    Welcome To <span id="W_Name1">GenBioSCope</span>
+                    Welcome To <span id="W_Name1">GenBioScope</span>
                   </h3>
                   <p>
-                    <span id="W_Name2">GenBioSCope</span> is a Professional{" "}
-                    <span id="W_Type1">Education</span> Platform. Here we will
-                    provide you only interesting content, which you will like
-                    very much. We're dedicated to providing you the best of{" "}
-                    <span id="W_Type2">Education</span>, with a focus on
-                    dependability and{" "}
-                    <span id="W_Spec">tutorial, learning, e-book, modular</span>
-                    . We're working to turn our passion for{" "}
-                    <span id="W_Type3">Education</span> into a booming{" "}
-                    <a
-                      href="https://www.blogearns.com"
-                      rel="do-follow"
-                      style={{ color: "inherit", textDecoration: "none" }}
-                    >
-                      online website
-                    </a>
-                    . We hope you enjoy our <span id="W_Type4">Education</span>{" "}
-                    as much as we enjoy offering them to you.
+                    <span id="W_Name2">
+                      GenBioScope is a web-based modular application
+                      thoughtfully designed for students and teachers. It is a
+                      study space to connect with classes, read and download
+                      modules, and do activities and quizzes completely for
+                      Biology subject.
+                    </span>
                   </p>
                   <p>
-                    I will keep posting more important posts on my Website for
-                    all of you. Please give your support and love.
+                    Further, GenBioScope comprises of designed Instructional
+                    Materials (IM's) that will aid in improving the learning
+                    process by addressing the incomprehensible and the least
+                    mastered competencies in Biology. This will help the
+                    students to comprehend the topic/s and improve their
+                    knowledge by using the Instructional Materials, Activities,
+                    and Quizzes included in this web-based modular application.
                   </p>
 
                   <p style={{ fontWeight: "bold", textAlign: "center" }}>
-                    Thanks For Visiting Our Site
+                    Thanks For Visiting Our Site.
                     <br />
                     <br />
                     <span
@@ -81,7 +75,7 @@ const About = () => {
                         textAlign: "center",
                       }}
                     >
-                      Have a nice day !
+                      Meet the Team.
                     </span>
                   </p>
                   <Row justify="center">
